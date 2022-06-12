09c6110df496b0d8b8b57bd5e403a28b061e014a
hyemmie <lhm0710@naver.com>
2022-06-13 00:58:33 +0900

    impl game2 view

--- game2.js
+++ game2.js
@@ -2,5 +2,122 @@ class Game2 extends Game {
   constructor(img_game, img_game_finished, img_back, img_game_info, img_game_success) {
     super(img_game, img_game_finished, img_back, img_game_info, img_game_success);
     this.button = new GameButton(this.img_button, this.img_finished_button, this.finished, 363, 230);
+    this.status = 1;
+    this.is_back = false;
+    this.score = 0;
   }
+
+  mousePressed(mouseX, mouseY) {
+    if (this.score < 100 && this.info_modal.isButtonOver(mouseX, mouseY)) {
+      clickSound.play();
+      this.ready = true;
+      this.info_modal.setVisible(!this.ready);
+    } else if (900 < mouseX && mouseX < 1000 && 500 < mouseY && mouseY < 600) {
+      clickSound.play();
+      this.score = 0;
+      stage = 1;
+      this.ready = false;
+    } else if (20 < mouseX && mouseX < 60 && 530 < mouseY && mouseY < 570) {
+      this.updateMic();
+    } else if (80 < mouseX && mouseX < 120 && 535 < mouseY && mouseY < 575) {
+      this.updateCam();
+    }
+  }
+
+  runGame(mouseX, mouseY) {
+    image(this.img_background, 0, 0);
+
+    if (this.status === 1 || this.status === 3) {
+    cam.loadPixels();
+      rectMode(CENTER);
+      for (let y = 100; y < CAM_IMAGE_HEIGHT; y += ps) {
+        for (let x = 0; x < CAM_IMAGE_WIDTH; x += ps) {
+          let index = (y * cam.width + cam.width - x) * 4;
+          let r = cam.pixels[index + 0];
+          let g = cam.pixels[index + 1];
+          let b = cam.pixels[index + 2];
+          fill(r, g, b);
+          noStroke();
+          rect(62 + x, 100 + y, ps, ps);
+        }
+      }
+    }
+      
+    if (this.is_back) {
+      image(img_prof_back, 60, 71);
+    }
+    this.drawMicAndCamByStatus();
+
+    this.info_modal.setVisible(!this.ready);
+    this.info_modal.show(mouseX, mouseY);
+  }
+
+  drawBackground() {
+    rectMode(CORNER);
+    fill(255);
+    noStroke();
+    rect(761, 84, 235, 35);
+  }
+
+  drawMicAndCamByStatus() {
+    this.drawBackground();
+    switch (this.status) {
+      case 0:
+        image(img_offoff, 761, 84);
+        image(img_mic_off, 20, 530);
+        image(img_cam_off, 80, 535);
+        break;
+      case 1:
+        image(img_offon, 761, 84);
+        image(img_mic_off, 20, 530);
+        image(img_cam_on, 80, 535);
+        break;
+      case 2:
+        image(img_onoff, 761, 84);
+        image(img_mic_on, 20, 530);
+        image(img_cam_off, 80, 535);
+        break;
+      case 3:
+        image(img_onon, 761, 84);
+        image(img_mic_on, 20, 530);
+        image(img_cam_on, 80, 535);
+        break;
+    }
+  }
+
+  updateMic() {
+    switch (this.status) {
+      case 0:
+        this.status = 2;
+        break;
+      case 1:
+        this.status = 3;
+        break;
+      case 2:
+        this.status = 0;
+        break;
+      case 3:
+        this.status = 1;
+        break;
+    }
+  }
+
+  updateCam() {
+    switch (this.status) {
+      case 0:
+        this.status = 1;
+        break;
+      case 1:
+        this.status = 0;
+        break;
+      case 2:
+        this.status = 3;
+        break;
+      case 3:
+        this.status = 2;
+        break;
+    }
+  }
+
+
 }
--- sketch.js
+++ sketch.js
@@ -3,19 +3,33 @@ let height = 600;
 
 let stage = 0;
 
-let img_game1_back;
-
 let img_opening;
 let img_gamelist;
 
+/* images for game 1 */
 let img_game1;
 let img_game1_finished;
 let img_game1_info;
 let img_game1_success;
+let img_game1_back;
 
+/* images for game 2 */
 let img_game2;
 let img_game2_finished;
 let img_game2_info;
+let img_game2_success;
+let img_game2_back;
+let img_onon;
+let img_onoff;
+let img_offon;
+let img_offoff;
+let img_prof_back;
+let img_mic_on;
+let img_mic_off;
+let img_cam_on;
+let img_cam_off;
+
+/* images for game 3 */
 let img_game3;
 let img_game3_finished;
 let img_game3_info;
@@ -49,7 +63,7 @@ function setup() {
   cam.hide();
   pixelDensity(1);
   let game1 = new Game1(img_game1, img_game1_finished, img_game1_back, img_game1_info, img_game1_success);
-  let game2 = new Game2(img_game2, img_game2_finished, img_game1_back, img_game1_info, img_game1_success);
+  let game2 = new Game2(img_game2, img_game2_finished, img_game2_back, img_game2_info, img_game2_success);
   let game3 = new Game3(img_game3, img_game3_finished, img_game1_back, img_game1_info, img_game1_success);
   game_list.push(game1);
   game_list.push(game2);
@@ -65,18 +79,31 @@ function preload() {
   clickSound = loadSound('sound/click');
   successSound = loadSound('sound/success');
 
-  img_game1_back = loadImage("./image/game1_back.png");
+  img_onon = loadImage("./image/onon.png");
+  img_onoff = loadImage("./image/onoff.png");
+  img_offon = loadImage("./image/offon.png");
+  img_offoff = loadImage("./image/offoff.png");
+  img_prof_back = loadImage("./image/prof_back.png");
 
   img_opening = loadImage("./image/opening.png");
   img_gamelist = loadImage("./image/gamelist.png");
+  
   img_game1 = loadImage("./image/game1.png");
   img_game1_finished = loadImage("./image/game1_finished.png");
   img_game1_info = loadImage("./image/game1_info.png");
   img_game1_success = loadImage("./image/game1_success.png");
+  img_game1_back = loadImage("./image/game1_back.png");
   img_glasses = loadImage("./image/glasses.png");
 
   img_game2 = loadImage("./image/game2.png");
   img_game2_finished = loadImage("./image/game2_finished.png");
+  img_game2_info = loadImage("./image/game2_info.png");
+  img_game2_success = loadImage("./image/game2_success.png");
+  img_game2_back = loadImage("./image/game2_back.png");
+  img_mic_on = loadImage("./image/mic_on.png");
+  img_mic_off = loadImage("./image/mic_off.png");
+  img_cam_on = loadImage("./image/cam_on.png");
+  img_cam_off = loadImage("./image/cam_off.png");
 
   img_game3 = loadImage("./image/game3.png");
   img_game3_finished = loadImage("./image/game3_finished.png");
@@ -135,15 +162,9 @@ function gamelist() {
 function mousePressed() {
   switch (stage) {
     case 0 :
-      if (355 < mouseX && mouseX < 645 && 469 < mouseY && mouseY < 546) { 
-        clickSound.play();
-        stage = 1;
-      }
+      opening_pressed();
       break;
     case 1 : 
-      for (let i = 0; i < game_list.length; i++) {
-        game_list[i].setReady(false);
-      }
       gamelist_pressed();
       break;
     case 2:
@@ -161,8 +182,17 @@ function mousePressed() {
   }
 }
 
+function opening_pressed() {
+  if (355 < mouseX && mouseX < 645 && 469 < mouseY && mouseY < 546) { 
+    clickSound.play();
+    stage = 1;
+  }
+}
+
 function gamelist_pressed() {
-    for (let i = 0; i < game_list.length; i++) {
+  for (let i = 0; i < game_list.length; i++) {
+    /* refresh games */
+    game_list[i].setReady(false);
     if (game_list[i].getButton().isOver(mouseX, mouseY)) {
       clickSound.play();
       stage = i + 2;
